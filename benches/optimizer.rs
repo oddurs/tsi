8a76b1b64ce0e6f5df730555f0a0acf672bbb218
Oddur Sigurdsson <oddurs@gmail.com>
2026-01-15 08:02:28 -0500

    Add benchmark placeholder

---
+++ benches/optimizer.rs
@@ -0,0 +1,8 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+
+fn optimizer_benchmarks(_c: &mut Criterion) {
+    // Placeholder for optimizer benchmarks
+}
+
+criterion_group!(benches, optimizer_benchmarks);
+criterion_main!(benches);
